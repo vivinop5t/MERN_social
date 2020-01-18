e832724a1c8ec0266499f43e53c5fc82398ea7a3
vivin36 <vivinop5t@gmail.com>
2020-01-18 18:56:59 +0530

    Db config

--- config/db.js
+++ config/db.js
@@ -4,7 +4,10 @@ const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db);
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true
+    });
   } catch (err) {
     console.log(err);
     process.exit(1);
