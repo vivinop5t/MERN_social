da7e2895171882b3bd34321e409393b546c7fc46
vivin36 <vivinop5t@gmail.com>
2020-01-18 18:54:10 +0530

    Added Routes

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.get("/", (req, res) => res.send("API running"));
 connectDb();
 const PORT = process.env.port || 5000;
 
+// middleware
+app.use(express.json({ extended: false }));
+
+//Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profiles", require("./routes/api/profiles"));
+app.use("/api/auth", require("./routes/api/authentication"));
+
 app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`);
 });
