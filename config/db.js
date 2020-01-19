b4ce12c620de576f43da6863e6523fcd9bd6e911
vivin36 <vivinop5t@gmail.com>
2020-01-19 13:35:45 +0530

    Changed To local

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    await mongoose.connect("mongodb://localhost/myapp", {
       useNewUrlParser: true,
       useCreateIndex: true
     });
